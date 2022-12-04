e2128584a2cc4ed3288eda0b6fc4805f12e81d7f
PhamVanDiep <phamdiepa1k55@gmail.com>
2022-12-04 09:55:18 +0700

    upload image config

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { View } from "react-native-ui-lib";
 import Login from "./src/test/login";
 import Register from "./src/views/register";
+import { UploadImage } from "./src/test/upload";
 
 export default function App() {
   return(
     <View>
-      <Register></Register>
-      <Login></Login>
+      {/* <Register></Register> */}
+      {/* <Login></Login> */}
+      <UploadImage/>
     </View>
   );
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,7 @@
-export const BASE_URL = 'http://10.0.2.2:8000/api/v1/'
+export const BASE_URL = 'http://10.0.2.2:8000/api/v1/';
+export const FIREBASE_CONFIG = {
+    BASE_URL: '',
+    AVATAR_STORAGE: 'avatar',
+    IMAGES_STORAGE: 'images',
+    AVATAR_DEFAULT: 'avatar_default'
+}
