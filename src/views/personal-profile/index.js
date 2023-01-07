b91d66afb8b805212f05f71f6123586537d95334
VuNQGum <nguyenquangvu05042001@gmail.com>
2023-01-07 15:36:20 +0700

    register and create post

---
+++ src/views/personal-profile/index.js
@@ -0,0 +1,12 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const PersonalProfileScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>PersonalProfileScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default PersonalProfileScreen;
--- src/views/register.js
+++ src/views/register.js
@@ -1,191 +1,170 @@
-import React, {Component} from 'react';
-import {
-  TextInput,
-  StyleSheet,
-  ScrollView,
-  ActivityIndicator,
-  Alert,
-} from 'react-native';
-import {
-  Avatar,
-  Incubator,
-  View,
-  RadioGroup,
-  RadioButton,
-  Text,
-  Checkbox,
-  Colors,
-  Button,
-} from 'react-native-ui-lib';
+import React, { useEffect, useState } from 'react'
+import { Avatar, Incubator, View, RadioGroup, RadioButton, Text, Checkbox, Colors, Button, Icon } from 'react-native-ui-lib';
 import _ from 'lodash';
-import {registerUser} from '../store/auth/authSlice';
-import {useDispatch} from 'react-redux';
+import UserService from '../helper/services/UserService';
+const { TextField } = Incubator;
+const options = ['Nam', 'Nữ']
+const logo = require('../../assets/image/Logo.png');
 
-const {TextField} = Incubator;
-const options = ['Nam', 'Nữ'];
+const Register = () => {
+    const [name, setName] = useState('');
+    const [gender, setGender] = useState('Nam');
+    const [phonenumber, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPass, setConfirmPass] = useState('');
+    const [confirm, setConfirm] = useState(true);
 
-class Register extends Component {
-  constructor(props) {
-    super(props);
-  }
-  
-  state = {
-    name: '',
-    gender: 'Nam',
-    phonenumber: '',
-    password: '',
-    confirm: '',
-  };
 
-  renderRadioButton(value, text, props) {
-    return (
-      <View row marginR-10>
-        <RadioButton value={value} label={text} {...props} />
-      </View>
-    );
-  }
+    const [validName, setValidName] = useState(false);
+    const [validGender, setValidGender] = useState(true);
+    const [validPhone, setValidPhone] = useState(false);
+    const [validPass, setValidPass] = useState(false);
+    const [isCheckpass, setCheckpass] = useState(false);
 
-  register(event) {
-    // event.preventDefault();
+    const [validForm, setValidForm] = useState(false);
 
-    if (this.state.password !== this.state.confirm) {
-      console.log('Mật khẩu không trùng khớp');
-      return;
+    const register = (name, gender, phone, password) => {
+        const requestBody = {
+            username: name,
+            gender: gender,
+            phonenumber: phone,
+            password: password
+        };
+        UserService.register(requestBody)
+            .then(res => {
+                console.log(res.data.data)
+            })
+            .catch(err => console.log(err));
     }
 
-    const registerForm = {
-      email: this.state.email,
-      phonenumber: this.state.phonenumber,
-      password: this.state.password,
+    useEffect(() => {
+        if (validName && validGender && validPhone && validPass && isCheckpass && confirm) {
+            setValidForm(true);
+        } else {
+            setValidForm(false);
+        }
+        // console.log("validForm", validForm);
+    }, [validName, validGender, validPhone, validPass, isCheckpass, confirm])
+
+    useEffect(() => {
+        if (confirmPass != password) {
+            setCheckpass(false);
+        } else {
+            setCheckpass(true);
+        }
+    }, [confirmPass])
+
+    function RenderRadioButton(value, text, select, props) {
+        return (
+            <View row marginR-10>
+                <RadioButton value={value} label={text} selected={select} {...props} />
+            </View>
+        );
     };
 
-    useDispatch(registerUser(registerForm));
-
-    
-  }
-
-  render() {
     return (
-      <View centerV paddingH-20 paddingT-50>
-        <View centerH>
-          <Avatar
-            source={{
-              uri: 'https://lh3.googleusercontent.com/-cw77lUnOvmI/AAAAAAAAAAI/AAAAAAAAAAA/WMNck32dKbc/s181-c/104220521160525129167.jpg',
-            }}
-          />
-        </View>
-        <View>
-          <TextField
-            fieldStyle={{borderBottomWidth: 1, padding: 2}}
-            containerStyle={{border: 1, borderRadius: 10}}
-            placeholder="Họ tên"
-            floatingPlaceholder
-            onChangeText={value => this.setState({name: value})}
-            enableErrors
-            validate={['required', 'text']}
-            validateOnBlur
-            validationMessage={['Field is required', 'Name is invalid']}
-            maxLength={30}
-          />
-        </View>
-        <View row>
-          <RadioGroup
-            row
-            initialValue={this.state.value}
-            onValueChange={value => this.setState({gender: value})}>
-            <Text text70 marginR-20 $textDefault>
-              Giới tính:
-            </Text>
-            <View row>
-              {this.renderRadioButton('Nam', 'Nam', {marginLeft: 5})}
-              {this.renderRadioButton('Nữ', 'Nữ', {marginLeft: 5})}
-              {this.renderRadioButton('Khác', 'Khác', true)}
+        <View centerV paddingH-20 paddingT-50>
+            <View centerH>
+                <Avatar source={logo} size={80} />
             </View>
-          </RadioGroup>
-        </View>
-        <View>
-          <TextField
-            fieldStyle={{borderBottomWidth: 1, padding: 2}}
-            placeholder={'Số điện thoại'}
-            floatingPlaceholder
-            onChangeText={value => this.setState({phonenumber: value})}
-            enableErrors
-            validate={['required', 'number', value => value.length >= 10]}
-            validateOnBlur
-            validationMessage={[
-              'Field is required',
-              'Only numbers are valid',
-              'Phone number is too short',
-            ]}
-            maxLength={30}
-          />
-        </View>
-        <View>
-          <TextField
-            fieldStyle={{borderBottomWidth: 1, padding: 2}}
-            placeholder={'Mật khẩu'}
-            floatingPlaceholder
-            onChangeText={value => this.setState({password: value})}
-            enableErrors
-            validate={['required', value => value.length >= 6]}
-            validateOnBlur
-            validationMessage={['Field is required', 'Password is too short']}
-            maxLength={30}
-          />
-        </View>
+            <View>
+                <TextField
+                    fieldStyle={{ borderBottomWidth: 1, padding: 2 }}
+                    placeholder='Họ tên'
+                    floatingPlaceholder
+                    onChangeText={value => setName(value)}
+                    enableErrors
+                    validate={['required']}
+                    validateOnBlur
+                    validationMessage={['Trường bắt buộc']}
+                    onChangeValidity={(isValid) => { setValidName(isValid); console.log("valid name:", isValid); }}
+                    maxLength={30}
+                />
+            </View>
+            <View row>
+                <RadioGroup row initialValue={this.gender} onValueChange={value => setGender(value)}>
+                    <Text text70 marginR-20 $textDefault>
+                        Giới tính:
+                    </Text>
+                    <View row>
+                        {RenderRadioButton('Nam', 'Nam', true, { marginLeft: 5 })}
+                        {RenderRadioButton('Nữ', 'Nữ', false, { marginLeft: 5 })}
+                        {RenderRadioButton('Khác', 'Khác', false)}
+                    </View>
+                </RadioGroup>
+            </View>
+            <View>
+                <TextField fieldStyle={{ borderBottomWidth: 1, padding: 2 }}
+                    placeholder={'Số điện thoại'}
+                    floatingPlaceholder
+                    onChangeText={value => setPhone(value)}
+                    enableErrors
+                    validate={['required', 'number', (value) => value.length >= 9]}
+                    validateOnBlur
+                    validationMessage={['Trường bắt buộc', 'Điện thoại chỉ gồm số', 'Số điện thoại không hợp lệ']}
+                    onChangeValidity={(isValid) => { setValidPhone(isValid); console.log("phone", isValid); }}
+                    maxLength={30}
+                    keyboardType='numeric'
+                />
+            </View>
+            <View>
+                <TextField fieldStyle={{ borderBottomWidth: 1, padding: 2 }}
+                    placeholder={'Mật khẩu'}
+                    floatingPlaceholder
+                    onChangeText={value => setPassword(value)}
+                    enableErrors
+                    validate={['required', (value) => value.length >= 6]}
+                    validateOnBlur
+                    validationMessage={['Trường bắt buộc', 'Mật khẩu quá ngắn']}
+                    onChangeValidity={(isValid) => { setValidPass(isValid); console.log("pass", isValid); }}
+                    maxLength={30}
+                    secureTextEntry={true}
+                >
+                </TextField>
+                {/* <Icon source={{uri: 'https://github.com/wix/react-native-ui-lib/blob/master/demo/src/assets/icons/delete.png'}} size={10} tintColor={Colors.grey40}/> */}
+            </View>
 
-        <View>
-          <TextField
-            fieldStyle={{borderBottomWidth: 1, padding: 2}}
-            placeholder={'Xác nhận mật khẩu'}
-            floatingPlaceholder
-            onChangeText={() => console.log('changed')}
-            enableErrors
-            validate={[
-              'required',
-              'text',
-              value => value.length >= 6,
-              value => value === this.state.password,
-            ]}
-            validateOnBlur
-            validationMessage={[
-              'Field is required',
-              'Email is invalid',
-              'Password is too short',
-              'Confirmation password does not match',
-            ]}
-            maxLength={30}
-          />
-        </View>
+            <View>
+                <TextField fieldStyle={{ borderBottomWidth: 1, padding: 2 }}
+                    placeholder={'Xác nhận mật khẩu'}
+                    floatingPlaceholder
+                    onChangeText={(value) => setConfirmPass(value)}
+                    enableErrors
+                    validate={['required']}
+                    validateOnBlur
+                    validationMessage={['Trường bắt buộc']}
+                    maxLength={30}
+                    secureTextEntry={true}
+                />
+                {!isCheckpass && <Text red30>Confirm password doesn't match</Text>}
+                <Text></Text>
+            </View>
 
-        <View row marginT-10>
-          <Text marginR-10>
-            Tôi đông ý với <Text>điều khoản sử dụng</Text>
-          </Text>
-          <Checkbox
-            value={this.state.value2}
-            onValueChange={value2 => this.setState({value2})}
-            borderRadius={5}
-            size={25}
-            color={Colors.purple30}
-          />
-        </View>
+            <View row marginT-10>
+                <Text marginR-10>
+                    Tôi đông ý với <Text>điều khoản sử dụng</Text>
+                </Text>
+                <Checkbox
+                    value={confirm}
+                    onValueChange={value6 => setConfirm(value6)}
+                    borderRadius={5}
+                    size={25}
+                    color={Colors.purple30}
+                />
+            </View>
 
-        <View marginT-20>
-          <Button
-            borderRadius={50}
-            label="Đăng ký"
-            onPress={this.register}
-          />
-        </View>
+            <View marginT-20>
+                <Button borderRadius={50} disabled={!validForm} label="Đăng ký" onPress={() => register(name, gender, phonenumber, password)} />
+            </View>
 
-        <View centerH row marginT-20>
-          <Text>Bạn đã có tài khoản?</Text>
-          <Button marginL-10 text80 link label="Đăng nhập"></Button>
+            <View centerH row marginT-20>
+                <Text>
+                    Bạn đã có tài khoản?
+                </Text>
+                <Button marginL-10 text80 link label="Đăng nhập" onPress={() => navigation.navigate('LogIn')}></Button>
+            </View>
         </View>
-      </View>
     );
-  }
 }
 
 export default Register;
